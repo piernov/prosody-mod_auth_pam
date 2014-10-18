1f84d9a06547330f4ea9e94e8007179b762a904f
piernov <piernov@piernov.org>
2014-10-18 20:01:00 +0200

    Fix\n (don't know what/why, but it works)

--- mod_auth_pam.lua
+++ mod_auth_pam.lua
@@ -8,31 +8,33 @@ local posix = require "posix";
 local pam = require "pam";
 local new_sasl = require "util.sasl".new;
 
-function user_exists(username)
-  return not not posix.getpasswd(username);
+local provider = {}
+
+function provider.user_exists(username)
+	return not not posix.getpasswd(username);
 end
 
-function test_password(username, password)
-  local h, err = pam.start("xmpp", username, {
-    function (t)
-      if #t == 1 and t[1][1] == pam.PROMPT_ECHO_OFF then
-        return { { password, 0} };
-      end
-    end
-  });
-  if h and h:authenticate() and h:endx(pam.SUCCESS) then
-    return true, true;
-  end
-  return nil, true;
+function provider.test_password(username, password)
+	local h, err = pam.start("xmpp", username, {
+		function (t)
+			if #t == 1 and t[1][1] == pam.PROMPT_ECHO_OFF then
+				return { { password, 0} };
+			end
+		end
+	});
+	if h and h:authenticate() and h:endx(pam.SUCCESS) then
+		return true, true;
+	end
+	return nil, true;
 end
 
-function get_sasl_handler()
-  return new_sasl(module.host, {
-    plain_test = function(sasl, ...)
-      return test_password(...)
-    end
-  });
+function provider.get_sasl_handler()
+	return new_sasl(module.host, {
+		plain_test = function(sasl, ...)
+			return provider.test_password(...)
+		end
+	});
 end
 
-module:provides"auth";
+module:provides("auth", provider);
 
