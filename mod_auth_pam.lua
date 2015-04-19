c5573e79b4b55affdfb0e601d0a2c4bd50777606
piernov <piernov@piernov.org>
2015-04-19 12:29:22 +0200

    Refactor mod_auth_pam based on mod_auth_internal_hashed

--- mod_auth_pam.lua
+++ mod_auth_pam.lua
@@ -7,34 +7,50 @@
 local posix = require "posix";
 local pam = require "pam";
 local new_sasl = require "util.sasl".new;
+local my_host = module.host;
 
-local provider = {}
+function new_default_provider(host)
+	local provider = { name = "pam" };
+	log("debug", "initializing pam authentication provider for host '%s'", host);
 
-function provider.user_exists(username)
-	return not not posix.getpasswd(username);
-end
+	function provider.test_password(username, password)
+		log("debug", "test password '%s' for user %s at host %s", password, username, module.host);
 
-function provider.test_password(username, password)
-	local h, err = pam.start("xmpp", username, {
-		function (t)
-			if #t == 1 and t[1][1] == pam.PROMPT_ECHO_OFF then
-				return { { password, 0} };
+		local h, err = pam.start("xmpp", username, {
+			function (t)
+				if #t == 1 and t[1][1] == pam.PROMPT_ECHO_OFF then
+					return { { password, 0} };
+				end
 			end
+		});
+		if h and h:authenticate() and h:endx(pam.SUCCESS) then
+			return true, true;
 		end
-	});
-	if h and h:authenticate() and h:endx(pam.SUCCESS) then
-		return true, true;
+		return nil, "Auth failed.";
 	end
-	return nil, true;
-end
 
-function provider.get_sasl_handler()
-	return new_sasl(module.host, {
-		plain_test = function(sasl, ...)
-			return provider.test_password(...)
+	function provider.user_exists(username)
+		local account = posix.getpasswd(username);
+		if not account then
+			log("debug", "account not found for username '%s' at host '%s'", username, module.host);
+			return nil, "Auth failed. Invalid username";
 		end
-	});
+		return true;
+	end
+
+	function provider.get_sasl_handler(session)
+		local testpass_authentication_profile = {
+			plain_test = function(sasl, ...)
+				return provider.test_password(...)
+			end,
+			order = {"plain_test"},
+			session = session,
+			host = my_host
+		};
+		return new_sasl(module.host, testpass_authentication_profile);
+	end
+
+	return provider;
 end
 
-module:provides("auth", provider);
-
+module:add_item("auth-provider", new_default_provider(module.host));
